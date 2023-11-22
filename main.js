a8da38b9c139d9c6bbdc2aa27048b6a634124995
juliaferreiracampos <127865377+juliaferreiracampos@users.noreply.github.com>
2023-11-22 12:11:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function playAudio(id_do_som) {
     alert(id_do_som);
-    
+
+    const audio = document.getElementById(id_do_som);
+
+    if (audio) {
+        audio.play();
+    } else {
+        alert("Não foi possível reproduzir som!");
+    }
+
+
 }
